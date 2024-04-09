7f16f48e172b55b4f1c5def8a2fd58a3b69428f5
bugzigus <bzigus@gmail.com>
2024-04-09 14:16:45 -0400

    <3

--- TestPodcastApp/ContentView.swift
+++ TestPodcastApp/ContentView.swift
@@ -6,24 +6,41 @@
 //
 
 import SwiftUI
+import AVFoundation
 
+var playAudio: AVAudioPlayer?
 
 struct ContentView: View {
+    
     
     var body: some View {
         VStack {
             Image(systemName: "music.note")
+                .padding(.bottom)
                 .imageScale(.large)
                 .shadow(color: .blue, radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
             
-            Text("Podcast App")
+            Text("test")
+                .padding(.top)
             
-            
-            
-            
-            
+            Button(action: play) {
+                Label("Sign In", systemImage: "arrow.up")
+            }
+            .buttonStyle(.borderedProminent)
         }
-        .padding()
+        
+    }
+    
+    
+}
+func play() {
+    let path = Bundle.main.path(forResource: "example.mp3", ofType:nil)!
+    let url = URL(fileURLWithPath: path)
+    do {
+        playAudio = try AVAudioPlayer(contentsOf: url)
+    } catch {
+        
+        
     }
 }
 
