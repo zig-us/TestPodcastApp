c1af01f37831842c555094d9094fde1f3a94ee66
bugzigus <bzigus@gmail.com>
2024-05-07 16:46:17 -0400

    The basics - Fixed Audio Playback - Added Speed Adjustments - Working on added background play

--- TestPodcastApp/ContentView.swift
+++ TestPodcastApp/ContentView.swift
@@ -7,11 +7,19 @@
 
 import SwiftUI
 import AVFoundation
-
-var playAudio: AVAudioPlayer?
+var audioPlayer: AVAudioPlayer!
 
 struct ContentView: View {
     
+    @State var buttonPlayTitle = "Play"
+    @State private var speed = 100.0
+    @State private var isEditing = false
+    
+    enum Speed: String, CaseIterable, Identifiable {
+        case half, normal, onepointfive, two
+        var id: Self { self }
+    }
+    @State private var selectedSpeed: Speed = .normal
     
     var body: some View {
         VStack {
@@ -24,23 +32,83 @@ struct ContentView: View {
                 .padding(.top)
             
             Button(action: play) {
-                Label("PlayMusic", systemImage: "arrow.up")
+                Label(buttonPlayTitle, systemImage: "arrow.up")
             }
             .buttonStyle(.borderedProminent)
+            
+            List {
+                Picker("Speed", selection: $selectedSpeed) {
+                    Text("Half").tag(Speed.half)
+                    Text("Normal").tag(Speed.normal)
+                    Text("One and a Half").tag(Speed.onepointfive)
+                    Text("Two").tag(Speed.two)
+                }.onReceive([self.selectedSpeed].publisher.first()) { value in
+                    self.changeRate()
+                }
+            }
+            
+            
+                        
         }
         
     }
-    
-    
-}
-func play() {
-    let path = Bundle.main.path(forResource: "example", ofType:"mp3")!
-    let url = URL(fileURLWithPath: path)
-    do {
-        playAudio = try AVAudioPlayer(contentsOf: url)
-    } catch {
+    func changeRate() {
+        print("this worked")
+        guard audioPlayer != nil else {
+            return
+        }
+            if selectedSpeed == .half {
+                
+                audioPlayer.rate = 0.5
+            } else if selectedSpeed == .normal {
+                audioPlayer.rate = 1.0
+            } else if selectedSpeed == .onepointfive {
+                audioPlayer.rate = 1.5
+            } else if selectedSpeed == .two {
+                audioPlayer.rate = 2.0
+            }
+        
+        
         
     }
+    
+    func play() {
+        
+        if let audioPlayer = audioPlayer, audioPlayer.isPlaying {
+            audioPlayer.pause()
+            buttonPlayTitle = "Play"
+            return;
+        }
+        let url = Bundle.main.url(forResource: "example", withExtension: "mp3")
+        
+        guard url != nil else {
+            return;
+        }
+        
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url!)
+            audioPlayer.enableRate = true
+            audioPlayer.prepareToPlay()
+            do {
+                try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default, options: [.mixWithOthers, .allowAirPlay])
+                print("Playback OK")
+                try AVAudioSession.sharedInstance().setActive(true)
+                print("Session is Active")
+            } catch {
+                print(error)
+            }
+            audioPlayer.rate = 1.0
+            audioPlayer?.play()
+            
+            buttonPlayTitle = "Pause"
+        } catch {
+            print("\(error)")
+        }
+        
+        
+    }
+
 }
 
 #Preview {
